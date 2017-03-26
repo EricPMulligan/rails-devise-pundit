a50a79b5059ca25757c0d733210ff61a341d86a1
Eric Mulligan <eric.pierre.mulligan@gmail.com>
2017-03-26 16:07:23 -0400

    Created Labels in the database, added Pundit to Controller and fixed specs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228010617) do
+ActiveRecord::Schema.define(version: 20170326195339) do
+
+  create_table "labels", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name",       null: false
+    t.string   "colour",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "name", "colour"], name: "index_labels_on_user_id_and_name_and_colour", unique: true
+    t.index ["user_id"], name: "index_labels_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
